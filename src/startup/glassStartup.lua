3536fd334f0cbc8a0fc680201fcef0c7ea55fb50
helkarakse <nimcuron@gmail.com>
2013-12-03 21:10:14 +0800

    Added new startup file for glassLite

---
+++ src/startup/glassStartup.lua
@@ -0,0 +1,49 @@
+--[[
+ 
+    MacroStartup Version 1.1 Beta
+    Do not modify, copy or distribute without permission of author
+	Helkarakse, 20130614
+ 	
+ 	Changelog:
+ 	- 1.1 - Changed the startup code to pull latest copy from github instead of private server, 20131202
+]]--
+
+-- File array of github links
+local fileArray = {
+	{link = "https://raw.github.com/helkarakse/TickProfilerDisplay/master/src/functions.lua", file = "functions"},
+	{link = "https://raw.github.com/helkarakse/TickProfilerDisplay/master/src/json.lua", file = "json"},
+	{link = "https://raw.github.com/helkarakse/TickProfilerDisplay/master/src/parser.lua", file = "parser"},
+	{link = "https://raw.github.com/helkarakse/TickProfilerDisplay/master/src/glass/glassLite.lua", file = "glassLite"}
+}
+
+-- This filename is the file that will be executed
+local indexFile = "glassLite"
+
+-- Set to true to overwrite files
+local overwrite = true
+ 
+-- Helper function to pull latest file from server
+local function getProgram(link, filename)
+	print("Downloading '" .. filename .. "' file from server.")
+	
+	-- remove the file if override is true
+	if (overwrite == true) then
+		shell.run("rm " .. filename)
+	end
+	
+	-- get the latest copy
+	local data = http.get(link)
+	if data then
+    	print("File '" .. filename .. "' download complete.")
+		local file = fs.open(filename,"w")
+		file.write(data.readAll())
+		file.close()
+	end
+end
+ 
+-- download and start program
+for i = 1, #fileArray do
+	getProgram(fileArray[i].link, fileArray[i].file)
+end
+
+shell.run(indexFile)
