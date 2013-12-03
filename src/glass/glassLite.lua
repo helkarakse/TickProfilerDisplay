8c6b1f2b7cd5ea7fba25da37e6d6e99709f6c490
Shotexpert <sandergillissen@hotmail.com>
2013-12-03 15:07:09 +0100

    Added drawSanta function

--- src/glass/glassLite.lua
+++ src/glass/glassLite.lua
@@ -38,6 +38,29 @@ local function drawTps()
 	clockText.setScale(2)
 end
 
+local function drawSanta(inputX, inputY)
+--colors
+white = 0xffffff
+red = 0xFF0000
+
+--initial starting position
+x = inputX
+y = inputY
+
+--white parts
+box1 = bridge.addBox(x-2, y-11, 2, 2, white, 1)
+box2 = bridge.addBox(x-9, y-1, 9, 2, white, 1)
+
+--red parts
+box3 = bridge.addBox(x-2, y-8, 2, 1, red, 1)
+box4 = bridge.addBox(x-3, y-7, 4, 1, red, 1)
+box5 = bridge.addBox(x-4, y-6, 5, 1, red, 1)
+box6 = bridge.addBox(x-5, y-5, 5, 1, red, 1)
+box7 = bridge.addBox(x-6, y-4, 5, 1, red, 1)
+box8 = bridge.addBox(x-7, y-3, 6, 1, red, 1)
+box9 = bridge.addBox(x-8, y-2, 8, 1, red, 1)
+end
+
 local tpsRefreshLoop = function()
 	while true do
 		local file = fs.open(jsonFile, "r")
@@ -79,6 +102,7 @@ local function init()
 	drawMain()
 	drawHeader()
 	drawTps()
+	drawSanta(100,100)
 	
 	parallel.waitForAll(tpsRefreshLoop, clockRefreshLoop)
 end
