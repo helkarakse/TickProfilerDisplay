0e1fa415aa6494cfc34b8831e38f207d9a1d859b
helkarakse <nimcuron@gmail.com>
2014-01-09 02:29:52 +0800

    Updated the api url and pack, server parsing for the tick profiler

--- src/data.lua
+++ src/data.lua
@@ -6,5 +6,6 @@
 
 ]]
 
-local dimId = string.sub(os.getComputerLabel(), 1, 1)
-dataUrl = "http://dev.otegamers.com/helkarakse/tps/upload.php?req=show&output=json&dim=" .. dimId
+local server = string.sub(os.getComputerLabel(), 1, 1)
+local pack = string.lower(string.sub(os.getComputerLabel(), 2))
+dataUrl = "http://dev.otegamers.com/api/v1/tps/get/" .. $pack .. "/" .. $server
